4e89a8a7ebc66acbef8a5928c305fa0d002b8451
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-09-17 15:52:49 +0900

    Feat : Fuctional Routing

--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [],
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/teams',
+      component: TeamsList,
+    },
+    {
+      path: '/users',
+      component: UsersList,
+    },
+  ],
+  linkActiveClass: 'active',
 });
 
 const app = createApp(App);
+
+app.use(router);
 
 app.mount('#app');
